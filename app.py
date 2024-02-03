82a5c31e2c5e23ea2a7bf7a1daaaf1219ccd34c9
s2ahil <101473078+s2ahil@users.noreply.github.com>
2024-02-03 11:18:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,13 @@
 
 from taipy import Gui
 from flask import Flask, request, jsonify
-# import openai
 from taipy.gui import Html
 import re
 import os
 from PIL import Image
 import google.generativeai as palm
 from dotenv import load_dotenv
+import json
 load_dotenv() 
 
 
@@ -22,60 +22,78 @@ model = models[0].name
 image_path="./safe journey.gif"
 image = Image.open(image_path)
 
-def format_content(content):
-    # Define a regular expression pattern to identify headings
-    heading_pattern = re.compile(r'\*\*([^*]+)\*\*')
-    
-    # Replace each heading with a new line after it
-    formatted_content = heading_pattern.sub(r'\n\n\1 ', content)
 
-    return formatted_content
 
+def display_json_result(result):
+    user_friendly_format = ""
+    try:
+        json_data = json.loads(result)
+        for key, value in json_data.items():
+            user_friendly_format += f"\n {key}: {value['text']} \n\n "
+    except json.JSONDecodeError as e:
+        user_friendly_format = f"Error decoding JSON: {e}"
+
+    return user_friendly_format
 
 def submit_scenario(state):
     print("pahucha")
-    input_url_place=state.input_url_place
-    input_url_option=state.input_url_option
-    print(input_url_option+input_url_place)
-    data={
-          "place":input_url_place ,
-          "option":  input_url_option
+    input_url_place = state.input_url_place
+    input_url_option = state.input_url_option
+    print(input_url_option + input_url_place)
+    data = {
+        "place": input_url_place,
+        "option": input_url_option
     }
 
     place = data.get("place")
     option = data.get("option")
 
-    prompt = f"Imagine you're a seasoned travel writer crafting a captivating guide about {place}, highlighting {option}. Paint a vivid picture with your words, including practical tips, hidden gems, and unique experiences that make this place special.PLease structure output in summarized points."
-    completion = palm.generate_text(
-    model=model,
-    prompt=prompt,
-    temperature=0.7,
-    # The maximum length of the response
-    max_output_tokens=650,
-    )
+  
+    prompt = f"""
+    Imagine you're a seasoned travel writer crafting a captivating guide about {place}, highlighting {option}. 
+    Paint a vivid picture with your words, including practical tips, hidden gems, and unique experiences that make this place special. 
+    dont give long response of text make it  short and informative  about the place or thing user are looking at.
+    *Present each recommendation or insight as a separate numbered JSON object, containing a descriptive sentence within the 'text' key.* 
+
+    Follow the example format:
+
+    {{
+    "1": {{"text": "Your first recommendation here."}},
+    "2": {{"text": "Your second recommendation here."}},
+    ...
+    }}
+ 
+    Please ensure each recommendation is structured as a separate JSON object with a unique number and follows the 'text' key format. Be mindful of correct JSON syntax.
+    """
     
-
-    # print(completion.choices[0].message)
-
-
+    completion = palm.generate_text(
+        model=model,
+        prompt=prompt,
+        temperature=0.7,
+        # The maximum length of the response
+        max_output_tokens=1000,
+    )
 
     print('reached')
-    message = format_content(completion.result)
-    state.message = message
+    res = completion.result
+    json_string = res.replace("```", "").replace("json", "")
+    print(json_string)
+    user_friendly_message = display_json_result(json_string)
+    state.message = user_friendly_message
 
 
 
     
 
-    # return jsonify({"response": response.choices[0].text.strip()})
 
-
-   
 
 options = ["The best places to go?", "The best things to do?", "The best travel Tips","The best clothes to bring ?","The best Time to go"]
 
 input_url_place=""
 input_url_option=""
+
+
+
 message=""
 
 # Definition of the page
@@ -85,9 +103,10 @@ page = """
 
 .container-heading {
   
-  font-size: 7rem;
+  font-size: 5rem;
   display:flex;
   justify-content:center;
+   align-items:center;
    paddding:2rem;
   }
 
@@ -95,6 +114,7 @@ page = """
    display:flex;
   justify-content:center;
   align-items:center;
+  padding:2rem;
   }
 
 .center{
@@ -103,6 +123,15 @@ page = """
    paddding:2rem;
    margin:3rem;
    text-align:center;
+}
+
+.center-output{
+ display:flex;
+ justify-content:center;
+ width:100%;
+}
+.fullwidth{
+width:100% !important;
 }
 
 .form{
@@ -116,6 +145,7 @@ page = """
 
 .middleSection{
  display:flex;
+  flex-wrap: wrap;
  justify-content:center;
  gap:2rem;
  padding:"2rem"
@@ -130,15 +160,14 @@ margin-top:1rem;
  display:flex;
  justify-content:center;
  align-items:center;
+  flex-direction:column;
 }
 
 </style>
 
-<|container-heading|
-
-
-Wander  Wizard 🪄
-|>
+<|container-heading| 
+ Wander  Wizard 🪄 
+ |>
 
 
 <|desc| 
@@ -162,8 +191,13 @@ Wander  Wizard 🪄
 
 
 <|form| 
+
+
 Enter place name : <br></br>
 <|{input_url_place}|input|>
+  
+
+
 
 Enter option for your trip : 
 <|{input_url_option}|selector|lov={options}|dropdown|>
@@ -174,6 +208,7 @@ Or enter a custom option:
 <|buttonStyle|
 <|submit|button|on_action=submit_scenario|>
 |>
+
 
 
 
@@ -192,14 +227,8 @@ Or enter a custom option:
 
 
 
-
-
- 
-
-<|center|
-
-Ai :  <|{message}|text|>
-
+<|part|class_name=card mt1|
+<|{message}|input|multiline|not active|label= your message will appear here...|class_name=fullwidth|>
 |>
 
 
@@ -223,4 +252,4 @@ Made with 💖 by
 if __name__ =="__main__": 
 # Create a Gui object with our page content
  app=Gui(page=page)
- app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+ app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True,use_reloader=True)
